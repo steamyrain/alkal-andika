d528f7da99086678044847ffe526a354ac85ab3e
rain <rain@pop-os.localdomain>
2020-12-05 09:05:22 +0700

    models: view laporan for specific operator

--- application/models/LapKerjaModel.php
+++ application/models/LapKerjaModel.php
@@ -24,6 +24,23 @@
             return $this->db->get();
         }
 
+        public function getOperatorsDataLaporan(int $uid) {
+            $this->db->select(
+                $this->table.'.id,'.
+                $this->table.'.plate_number,'.
+                $this->table.'.serial_number,'.
+                $this->table.'.km_onStart,'.
+                $this->table.'.km_onFinish,'.
+                $this->table.'.km_total,'.
+                $this->table.'.gasoline,'.
+                $this->table.'.project_location,'.
+                'user.username'
+            );
+            $this->db->from($this->table);
+            $this->db->where($this->table.'.userId ='.$uId);
+            return $this->db->get();
+        }
+
         // public method to insert data to table
         public function setDataLaporan($data) {
            $this->db->insert($this->table,$data); 
