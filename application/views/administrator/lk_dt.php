dfdea14d6d3724b869097974028d9bc70b43cab9
rain <rain@pop-os.localdomain>
2020-12-07 14:40:21 +0700

    view: add laporan kerja dump truck table view

---
+++ application/views/administrator/lk_dt.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+    <div class="alert alert-success" role="alert">
+        <i class="fas fa-clipboard"></i> Laporan Kerja Dump Truck 
+    </div>
+    <?php 
+        echo anchor(
+            base_url('administrator/laporandt/input'),
+            '<button class="btn btn-sm btn-primary mb-3">
+                <i class="fas fa-plus fa-sm"></i> 
+                Tambah Data
+            </button>'
+        ) 
+    ?>
+    <div style="overflow-x:auto;">
+    <table id="data-tabel" class="table table-bordered table-striped" style="width:100%">
+    <thead>
+  	<tr>
+  		<th class="text-center">ID</th>
+  		<th class="text-center">Nama</th>
+  		<th class="text-center">Lokasi Kerja</th>
+  		<th class="text-center">Nomer Polisi</th>
+  		<th class="text-center">KM Awal</th>
+  		<th class="text-center">KM Akhir</th>
+  		<th class="text-center">KM Total</th>
+  		<th class="text-center">BBM</th>
+    </tr>
+    <?php $i=1;foreach($laporan as $l):?>
+    </thead>
+    <tbody>
+    <tr>
+        <th class="text-center"><?php echo $l->id; ?></th>
+        <th class="text-center"><?php echo $l->username; ?></th>
+        <th class="text-center"><?php echo $l->project_location; ?></th>
+        <th class="text-center"><?php echo $l->plate_number; ?></th>
+        <th class="text-center"><?php echo $l->km_onStart; ?></th>
+        <th class="text-center"><?php echo $l->km_onFinish; ?></th>
+        <th class="text-center"><?php echo $l->km_total; ?></th>
+        <th class="text-center"><?php echo $l->gasoline; ?></th>
+    </tr>
+    <?php endforeach;?>
+        </tbody>
+  </table>
+</div>
+</div>
