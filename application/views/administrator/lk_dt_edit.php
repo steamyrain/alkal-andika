3fe647502fc6de5b1d82eef97453f5bc7c9e0b8c
rain <rain@pop-os.localdomain>
2020-12-08 09:29:32 +0700

    view: tidy up lk_dt_edit view

--- application/views/administrator/lk_dt_edit.php
+++ application/views/administrator/lk_dt_edit.php
@@ -1,11 +1,10 @@
 <div class="container-fluid">
-
-	    <?php echo form_open('administrator/laporandt/input_aksi'); ?> 
+	    <?php echo form_open('administrator/laporandt/edit_aksi'); ?> 
 		<div class="form-group">
             <label> Nama :</label>
 			<select name="uId" class="form-control">
             <?php $no=0; foreach ($username as $uname):?>
-                <option value="<?php echo $oId[$no]; ?>"><?php echo $uname; ?></option>
+                <option value="<?php echo $oId[$no]; ?>" <?php echo ($record->userId == $oId[$no])?'selected':''; ?>><?php echo $uname; ?></option>
                 <?php $no++; ?>
             <?php endforeach; ?>
             </select>
@@ -16,9 +15,9 @@
 			<label>Nomor Polisi :</label>
 			<select name="plate_number" class="form-control">
                 <option selected="selected">NULL</option>
-            <?php foreach ($plate_number as $pn):?>
-                <option><?php echo $pn; ?></option>
-            <?php endforeach; ?>
+                <?php foreach ($plate_number as $pn):?>
+                    <option <?php echo ($record->plate_number == $pn)?'selected':''; ?>><?php echo $pn; ?></option>
+                <?php endforeach; ?>
             </select>
             <?php echo form_error('plate_number', '<div class="text-danger small" ml-3>') ?>
 		</div>
@@ -26,9 +25,11 @@
 		<div class="form-group">
 			<label>Lokasi Kerja :</label>
 			<input type="text" name="project_location"
+            value="<?php echo $record->project_location; ?>"
 			placeholder="Masukkan Lokasi Kerja" class="form-control">
 			<?php echo form_error('project_location', '<div class="text-danger small" ml-3>') ?>
 		</div>
+
 		<div class="form-group">
 			<label>km/hm awal :</label>
             <input 
@@ -36,9 +37,11 @@
                 name="lk__km_onStart"
                 placeholder="Masukkan KM Awal"
                 class="form-control"
+                value="<?php echo $record->km_onStart; ?>"
             >
 			<?php echo form_error('lk__km_onStart', '<div class="text-danger small" ml-3>') ?>
         </div>
+
         <div class="form-group">
             <label>km/hm akhir :</label>
             <input 
@@ -46,17 +49,22 @@
                 name="lk__km_onFinish"
                 placeholder="Masukkan KM Akhir" 
                 class="form-control"
+                value="<?php echo $record->km_onFinish; ?>"
             >
 			<?php echo form_error('lk__km_onFinish', '<div class="text-danger small" ml-3>') ?>
 		</div>
+
 		<div class="form-group">
 			<label>Jarak :</label>
 			<input id="lk__jarak" type="text" name="km_total"
+            value="<?php echo $record->km_total; ?>"
             placeholder="KM Awal - KM Akhir" class="form-control" readonly>
 		</div>
+
 		<div class="form-group" style="display: none;">
 			<label>BBM :</label>
 			<input type="text" name="gasoline"
+            value="<?php echo $record->gasoline; ?>"
 			placeholder="Masukkan BBM" class="form-control">
 			<?php echo form_error('gasoline', '<div class="text-danger small" ml-3>') ?>
 		</div>
