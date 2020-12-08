4ee5549585b7e9d6fe5cc0d0022579d5bb9b0f95
rain <rain@pop-os.localdomain>
2020-12-08 08:14:30 +0700

    view: fix bugs for delete button by changing id on each form

--- application/views/administrator/laporan_kerja.php
+++ application/views/administrator/laporan_kerja.php
@@ -14,53 +14,53 @@
     <div style="overflow-x:auto;">
     <table id="data-tabel" class="table table-bordered table-striped" style="width:100%">
     <thead>
-  	<tr>
-  		<th class="text-center">Aksi</th>
-  		<th class="text-center">Tanggal</th>
-  		<th class="text-center">Nama</th>
-  		<th class="text-center">Lokasi Kerja</th>
-  		<th class="text-center">Nomer Polisi</th>
-  		<th class="text-center">Nomer Seri</th>
-  		<th class="text-center">KM Awal</th>
-  		<th class="text-center">KM Akhir</th>
-  		<th class="text-center">KM Total</th>
-  		<th class="text-center">BBM</th>
-    </tr>
-    <?php $i=1;foreach($laporan as $l):?>
+        <tr>
+            <th class="text-center">Aksi</th>
+            <th class="text-center">Tanggal</th>
+            <th class="text-center">Nama</th>
+            <th class="text-center">Lokasi Kerja</th>
+            <th class="text-center">Nomer Polisi</th>
+            <th class="text-center">Nomer Seri</th>
+            <th class="text-center">KM Awal</th>
+            <th class="text-center">KM Akhir</th>
+            <th class="text-center">KM Total</th>
+            <th class="text-center">BBM</th>
+        </tr>
     </thead>
     <tbody>
+    <?php foreach($laporan as $l):?>
     <tr>
         <td>
-        <div 
-            class="aksi" 
-            style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 5px;"
-        >
-            <form 
-                style="display: none;" 
-                id="form-hapus" 
-                method="post" 
-                action="<?php echo base_URL('administrator/laporan/hapus_aksi')?>"
+            <div 
+                class="aksi" 
+                style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 5px;"
             >
-                <input type="text" name="id" value="<?php echo $l->id ?>"/>
-            </form>
-            <a 
-                onclick="document.getElementById('form-hapus').submit()"
-            >
-                <div 
-                    class="btn btn-danger btn-sm" 
-                    onclick="javascript: return confirm('Yakin Hapus?')"
+                <form 
+                    style="display: none;" 
+                    id="form-hapus-<?php echo $l->id; ?>" 
+                    method="post" 
+                    action="<?php echo base_URL('administrator/laporan/hapus_aksi')?>"
                 >
-                    <i class="fa fa-trash"></i>
-                </div>
-            </a>
-            <a 
-                href="<?php echo base_URL('administrator/laporan/edit/'.$l->id)?>"
-            >
-                <div class="btn btn-warning btn-sm">
-                    <i class="fa fa-edit"></i>
-                </div>
-            </a>
-        </div>
+                    <input type="text" name="id" value="<?php echo $l->id; ?>">
+                </form>
+                <a 
+                onclick="document.getElementById('form-hapus-<?php echo $l->id; ?>').submit()"
+                >
+                    <div 
+                        class="btn btn-danger btn-sm" 
+                        onclick="javascript: return confirm('Yakin Hapus?')"
+                    >
+                        <i class="fa fa-trash"></i>
+                    </div>
+                </a>
+                <a 
+                    href="<?php echo base_URL('administrator/laporan/edit/'.$l->id); ?>"
+                >
+                    <div class="btn btn-warning btn-sm">
+                        <i class="fa fa-edit"></i>
+                    </div>
+                </a>
+            </div>
         </td>
         <td class="text-center"><?php echo $l->created_at; ?></td>
         <td class="text-center"><?php echo $l->username; ?></td>
