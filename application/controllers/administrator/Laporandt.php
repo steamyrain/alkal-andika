2ba8bd867b76343c68a9195aa3b8c5d7e2b575ae
rain <rain@pop-os.localdomain>
2020-12-08 09:34:58 +0700

    controller: fix edit method

--- application/controllers/administrator/Laporandt.php
+++ application/controllers/administrator/Laporandt.php
@@ -64,18 +64,17 @@
             // Populate username for form field username
             $operators = $this->populateOperator();
             // Populate vin for form field plate_number and serial_number
-            $vin = $this->populateVin();
+            $plate_number = $this->populateVin();
             $record = $this->LKDTModel->getDataLaporanSpecific($lapId)->row();
             $data = [
                 'username'=>$operators['username'],
                 'oId'=>$operators['oId'],
-                'plate_number'=>$vin['plate_number'],
-                'serial_number'=>$vin['serial_number'],
+                'plate_number'=>$plate_number,
                 'record'=>$record
             ];
             $this->load->view('template_administrator/header');
             $this->load->view('template_administrator/sidebar');
-            $this->load->view('administrator/laporan_kerja_edit',$data);
+            $this->load->view('administrator/lk_dt_edit',$data);
             $this->load->view('template_administrator/footer');
         }
 
