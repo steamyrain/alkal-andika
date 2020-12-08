7796f9acc9798bf3e74b80c7125bc7e79521cd20
rain <rain@pop-os.localdomain>
2020-12-08 14:06:31 +0700

    controller: add admin login checker

--- application/controllers/administrator/Alatberat.php
+++ application/controllers/administrator/Alatberat.php
@@ -1,20 +1,32 @@
 <?php 
 
 class Alatberat extends CI_Controller {
-	function __construct(){
-		parent::__construct();
+    private function is_loggedIn() {
+        if (!isset($this->session->userdata['username'])){
+            $this->session->set_flashdata('pesan','<div class="alert alert-warning alert-danger dismissible fade show" role="alert">
+                Anda Belum Login!
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                 <span aria-hidden="true">&times;</span>
+                 </button>
+                </div>');
+            redirect('administrator/auth');
+        }
+    }
 
-		if (!isset($this->session->userdata['username'])){
-			$this->session->set_flashdata('pesan','<div class="alert alert-warning alert-danger dismissible fade show" role="alert">
-  				Anda Belum Login!
- 				 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-   				 <span aria-hidden="true">&times;</span>
- 				 </button>
-				</div>');
-			redirect('administrator/auth');
-		}
-	}
+    private function is_admin() {
+        if($this->session->userdata['level'] !== 'admin'){
+            $this->session->set_flashdata('pesan','<div class="alert alert-warning alert-danger dismissible fade show" role="alert">
+                Anda Belum Login!
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                 <span aria-hidden="true">&times;</span>
+                 </button>
+                </div>');
+            redirect('administrator/auth');
+        }
+    }
     public function index() {
+        $this->is_loggedIn();
+        $this->is_admin();
         $alatBerat = $this->AlatBeratModel->tampilAlatDanMerek()->result();
         $data = [
             'alatBerat'=> $alatBerat
