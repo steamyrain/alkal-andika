0724ca82c7ed40ad0c746a852a7c56dfd6457f87
rain <rain@pop-os.localdomain>
2020-12-11 13:47:17 +0700

    model: add printAllFormat method

--- application/models/LapKerjaModel.php
+++ application/models/LapKerjaModel.php
@@ -74,5 +74,25 @@
         public function deleteLaporan($id) {
            $this->db->delete($this->table,array('id'=>$id)); 
         }
+
+        public function printAllFormat() {
+            $this->db->select(
+                $this->table.'.created_at,'.
+                $this->table.'.plate_number,'.
+                $this->table.'.serial_number,'.
+                $this->table.'.km_onStart,'.
+                $this->table.'.km_onFinish,'.
+                $this->table.'.km_total,'.
+                $this->table.'.gasoline,'.
+                $this->table.'.project_location,'.
+                $this->table.'.userId,'.
+                'user.username, alkal_alat_berat.sub_category, alkal_category_alat_berat.category'
+            );
+            $this->db->from($this->table);
+            $this->db->join('user',$this->table.'.userId = user.id');
+            $this->db->join('alkal_alat_berat',$this->table.'.serial_number = alkal_alat_berat.serial_number or '.$this->table.'.plate_number = alkal_alat_berat.plate_number');
+            $this->db->join('alkal_category_alat_berat','alkal_category_alat_berat.id = alkal_alat_berat.catId');
+            return $this->db->get();
+        }
     }
 ?>
