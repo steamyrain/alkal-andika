63f0f3ebda9b22d3a78fcdcc36f149b4912d627a
rain <rain@pop-os.localdomain>
2021-01-08 14:21:51 +0700

    model: add surat tugas model

--- application/models/SuratTugasModel.php
+++ application/models/SuratTugasModel.php
@@ -1,7 +1,25 @@
 <?php 
 class SuratTugasModel extends CI_Model {
+
     private $table='alkal_surat_tugas';
+    private $tableSubject = 'alkal_st_subject';
+    private $tableHeavy = 'alkal_st_heavy';
+    private $tableDT = 'alkal_st_dt';
+
     public function insertSuratTugas($data) {
         $this->db->insert($this->table,$data);
+        return $this->db->insert_id();
+    }
+
+    public function insertSTSubject($data) {
+        $this->db->insert_batch($this->tableSubject,$data);
+    }
+
+    public function insertSTHeavy($data) {
+        $this->db->insert_batch($this->tableHeavy,$data);
+    }
+
+    public function insertSTDT($data) {
+        $this->db->insert_batch($this->tableDT,$data);
     }
 }
