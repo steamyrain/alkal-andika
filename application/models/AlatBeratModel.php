a6807988bdadcbb3f4ecd684eeb6e7f5cb051717
rain <rain@pop-os.localdomain>
2021-01-14 08:38:51 +0700

    model: fix tampil alat dan merek

--- application/models/AlatBeratModel.php
+++ application/models/AlatBeratModel.php
@@ -24,9 +24,6 @@ class AlatBeratModel extends CI_Model {
             'alkal_category_alat_berat.id='
             .$this->table.'.catId'
         );
-        $this->db->where(
-            $this->table.'.id=1'
-        );
         return $this->db->get();
     }
     public function getPlateAndSerial() {
