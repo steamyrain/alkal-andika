e70ea0bdebc0056b88fa0d34bf688cd0eeeb205a
rain <rain@pop-os.localdomain>
2021-02-03 10:57:05 +0700

    view: change print button now use form first

--- application/views/administrator/kinerja.php
+++ application/views/administrator/kinerja.php
@@ -15,7 +15,7 @@
     ?>
     <?php
     echo anchor(
-        'administrator/kinerja/print',
+        'administrator/kinerja/print_form',
         '<button class="btn btn-sm btn-info mb-3">
             <i class="fa fa-print"></i> 
             Print
