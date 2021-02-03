2a4c4eaf4bd97fc235a552169fbbab3985648877
u8718924_alkal <u8718924@srv88.niagahoster.com>
2021-02-03 11:15:08 +0700

    changes by andika

--- application/views/template_administrator/sidebar.php
+++ application/views/template_administrator/sidebar.php
@@ -179,7 +179,7 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">welcome</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Welcome, <?php echo $this->session->userdata('username') ?></span>
                 <img class="img-profile rounded-circle" src="<?php echo base_url() ?>assets/img/BM.png" alt="bm" width="60">
 
                 </a>
