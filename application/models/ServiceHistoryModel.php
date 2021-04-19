1c2360b7fb4517794add532103383991827f8c7b
rain <rain@pop-os.localdomain>
2021-04-19 20:51:06 +0700

    change table name

--- application/models/ServiceHistoryModel.php
+++ application/models/ServiceHistoryModel.php
@@ -6,7 +6,7 @@ class ServiceHistoryModel extends CI_Model {
             $this->db->select('b.plate_number as plate_number,a.dt_id as dt_id,a.service_id as service_id,a.serviced_by as serviced_by,c.service_name as service_name,a.service_date as service_date');
             $this->db->from('alkal_service_history_dt a');
             $this->db->join('alkal_dump_truck b','b.id = a.dt_id');
-            $this->db->join('alkal_service_list c','a.service_id = c.id');
+            $this->db->join('alkal_service_list_dt c','a.service_id = c.id');
             return $this->db->get();
         }
         else {
@@ -17,7 +17,7 @@ class ServiceHistoryModel extends CI_Model {
     public function getDTServiceList($id){
         if (isset($id) && !($id)){
             $this->db->select('a.id as id, a.service_name as service_name');
-            $this->db->from('alkal_service_list a');
+            $this->db->from('alkal_service_list_dt a');
             $this->db->join('alkal_service_dt_lookup b','b.service_id = a.id');
             $this->db->join('alkal_category_dt_service_lookup c','c.category_service_id = b.category_service_id');
             $this->db->join('alkal_dump_truck d','d.catId = c.category_id');
@@ -25,7 +25,7 @@ class ServiceHistoryModel extends CI_Model {
             return $this->db->get();
         } else {
             $this->db->select('a.id as id, a.service_name as service_name');
-            $this->db->from('alkal_service_list a');
+            $this->db->from('alkal_service_list_dt a');
             return $this->db->get();
         }
     }
